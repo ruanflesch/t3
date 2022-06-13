0b474e8a5c32d0e2adcdbdaca9722c4be0d5c17d
ruanflesch <68014923+ruanflesch@users.noreply.github.com>
2022-06-13 20:44:49 -0300

    Update ChromeTest.java

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -1,8 +1,3 @@
-
-package io.github.bonigarcia.wdm.test;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 
 import org.junit.After;
@@ -11,47 +6,45 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class ChromeTest {
 
-    private WebDriver driver;
+	private WebDriver driver;
 
-    @BeforeClass
-    public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
+	@BeforeClass
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
 
-    @Before
-    public void setupTest() {
-        driver = new ChromeDriver();
-    }
+	@Before
+	public void setupTest() {
+		driver = new ChromeDriver();
+	}
 
-    @After
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 
-    @Test
-    public void test() {
-        // Your test code here. For example:
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-        By searchInput = By.id("searchInput");
-        wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("Software");
-        By searchButton = By.id("searchButton");
-        wait.until(elementToBeClickable(searchButton));
-        driver.findElement(searchButton).click();
+	@Test
+	public void test() {
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("PUCRS");
+		element.submit();
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(textToBePresentInElementLocated(By.tagName("body"),
+                "Universidade"));
+		
+		System.out.println("O título da página é: " + driver.getTitle());
 
-        wait.until(textToBePresentInElementLocated(By.tagName("body"),
-                "Computer software"));
-    }
+	}
 
 }
