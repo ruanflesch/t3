a1cc9b1972a307a0105d65193cd5f15f273d6199
ruanflesch <68014923+ruanflesch@users.noreply.github.com>
2022-06-13 20:37:41 -0300

    new ChromeTest

---
+++ ChromeTest.java
@@ -0,0 +1,50 @@
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeTest {
+
+	private WebDriver driver;
+
+	@BeforeClass
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@Before
+	public void setupTest() {
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void test() {
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("PUCRS");
+		element.submit();
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(textToBePresentInElementLocated(By.tagName("body"),
+                "Universidade"));
+		
+		System.out.println("O título da página é: " + driver.getTitle());
+
+	}
+
+}
